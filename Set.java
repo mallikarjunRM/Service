6ddf215b1068013c0b593d26b74310195254cc6b
mallikarjunRM <rm_arjun@yahoo.in>
2015-05-16 04:42:13 -0700

    addind setfile

---
+++ Set.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+
+class Set {
+
+public static void main(String args[]){
+
+HashSet<Integer> hs=new HashSet<Integer>();
+
+hs.add(52);
+hs.add(25);
+hs.add(65);
+System.out.println(hs);
+hs.remove(25);
+System.out.println(hs);
+}
+
+
+
+}
